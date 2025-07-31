25e1f2bde912a141ff05800b38ac2db19b87d9ed
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-07-31 21:48:13 +0000

    build: :heavy_plus_sign: Add eslint import and yml support

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,25 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import pluginImport from 'eslint-plugin-import'
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      import: pluginImport,
+    },
+    rules: {
+      'import/order': ['error', {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      }],
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
