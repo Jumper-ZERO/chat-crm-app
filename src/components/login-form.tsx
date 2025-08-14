c7869e4773ec480a7b3e49e6515b621d82203495
AnthonyACL <alessandro_condori_2005@hotmail.com>
2025-08-14 02:09:35 +0000

    chore: :memo: Update texts in the login and forgot password forms

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -34,7 +34,7 @@ export function LoginForm({
           <form>
             <div className="flex flex-col gap-6">
               <div className="grid gap-3">
-                <Label htmlFor="email">{t("login.emailLabel")}</Label>
+                <Label htmlFor="email">{t("login.email.label")}</Label>
                 <Input
                   id="email"
                   type="email"
@@ -44,12 +44,12 @@ export function LoginForm({
               </div>
               <div className="grid gap-3">
                 <div className="flex items-center">
-                  <Label htmlFor="password">{t("login.passwordLabel")}</Label>
+                  <Label htmlFor="password">{t("login.password.label")}</Label>
                   <a
                     href="/forgot-password"
                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                   >
-                    {t("login.forgotPassword")}
+                    {t("login.password.forgot")}
                   </a>
                 </div>
                 <Input id="password" type="password" required />
@@ -64,9 +64,9 @@ export function LoginForm({
               </div>
             </div>
             <div className="mt-4 text-center text-sm">
-              {t("login.noAccountText")}{" "}
+              {t("login.link.noAccount")}{" "}
               <a href="/(auth)/register" className="underline underline-offset-4">
-                {t("login.registerLink")}
+                {t("login.link.register")}
               </a>
             </div>
           </form>
