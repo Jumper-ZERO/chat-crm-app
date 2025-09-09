7906c960792b624d1a7adcff6cb1fbde7f54ad93
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-09-09 21:08:22 +0000

    feat: :wrench: Add configuration for dayjs locale and format code syntax
    Changes minors in format code
    
    main.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,37 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import dayjs from "dayjs";
+import "dayjs/locale/es";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen'
+import { routeTree } from "./routeTree.gen";
 
-import { ThemeProvider } from '@/components/theme-provider'
-import { I18nProvider } from '@/lib/i18n/I18nProvider'
+import { ThemeProvider } from "@/components/theme-provider";
+import { I18nProvider } from "@/lib/i18n/I18nProvider";
+
+dayjs.locale("es"); // se aplica globalmente
 
 // Create a new router instance
 const router = createRouter({
   context: { user: null }, // Auth context
   routeTree,
-})
+});
 
 // Register the router instance for type safety
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <I18nProvider lang="es">
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <RouterProvider router={router} />
       </ThemeProvider>
     </I18nProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
