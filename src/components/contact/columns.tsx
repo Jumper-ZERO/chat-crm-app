57f7ce24bebfa703084f0d1565622e50ca7cc9a7
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-09-10 16:38:43 +0000

    style: :bulb: Drop code commented

--- src/components/contact/columns.tsx
+++ src/components/contact/columns.tsx
@@ -5,60 +5,6 @@ import { Badge } from "@/components/ui/badge";
 import { formatPhone } from "@/lib/phone";
 import type { Contact } from "@/models/contact.model";
 
-// export const columns: ColumnDef<Contact>[] = [
-//   {
-//     accessorKey: "name",
-//     header: "Nombre",
-//   },
-//   {
-//     accessorKey: "phone",
-//     header: "Teléfono",
-//     cell: ({ row }) => <p>{formatPhone(row.getValue("phone"))}</p>,
-//   },
-//   {
-//     accessorKey: "customerStatus",
-//     header: "Estado",
-//     cell: ({ row }) => {
-//       const status = row.getValue(
-//         "customerStatus"
-//       ) as Contact["customerStatus"];
-//       const colorMap: Record<Contact["customerStatus"], string> = {
-//         new: "bg-cyan-700",
-//         active: "bg-emerald-500",
-//         inactive: "text-gray-500",
-//         blocked: "text-red-600",
-//       };
-//       return (
-//         <Badge className={`${colorMap[status]} text-white font-bold`}>
-//           {status}
-//         </Badge>
-//       );
-//     },
-//   },
-//   {
-//     accessorKey: "source",
-//     header: "Origen",
-//     cell: ({ row }) => <p className="capitalize">{row.getValue("source")}</p>,
-//   },
-//   {
-//     accessorKey: "assignedTo",
-//     header: "Asignacion",
-//     cell: ({ row }) => {
-//       const assigned = row.getValue("assignedTo") as { username: string };
-//       return <p className="capitalize">{assigned.username}</p>;
-//     },
-//   },
-//   {
-//     accessorKey: "createdAt",
-//     header: "Creado",
-//     cell: ({ row }) => (
-//       <p className="capitalize">
-//         {dayjs(row.getValue("createdAt")).locale("es").format("MMMM D, YYYY")}
-//       </p>
-//     ),
-//   },
-// ];
-
 export const columns: ColumnDef<Contact>[] = [
   {
     accessorKey: "name",
