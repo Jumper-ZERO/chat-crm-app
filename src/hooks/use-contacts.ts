a2d53408565506f6aad3ef0ad1c3db1d4dc2e103
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-09-11 20:15:01 +0000

    feat: :sparkles: Add data-table hooks

--- src/hooks/use-contacts.ts
+++ src/hooks/use-contacts.ts
@@ -2,18 +2,10 @@ import { useQuery } from "@tanstack/react-query"
 
 import { getContacts } from "@/services/contact.service"
 
-// export const useContacts = () => {
-//   return useQuery({
-//     queryKey: ["contacts"],
-//     queryFn: getContacts,
-//   })
-// }
-
 export const useContacts = (page: number, limit: number = 10) => {
   return useQuery({
     queryKey: ["contacts", page, limit],
     queryFn: () => getContacts(page, limit),
     placeholderData: (previousData) => previousData,
-    // keepPreviousData: true, // mantiene los datos mientras carga la nueva página
   })
 }
