0774ad99412b2fb337ec16a594f8a6354aafffa8
Jeremi_aron <chancanjeremiaron@gmail.com>
2025-09-28 04:57:06 +0000

    fix: :lipstick: Fix with for showSubmittedData function helper

--- src/lib/show-submitted-data.tsx
+++ src/lib/show-submitted-data.tsx
@@ -6,9 +6,11 @@ export function showSubmittedData(
 ) {
   toast.message(title, {
     description: (
-      // w-[340px]
-      <pre className='mt-2 w-full overflow-x-auto rounded-md bg-slate-950 p-4'>
-        <code className='text-white'>{JSON.stringify(data, null, 2)}</code>
+      // * w-[320px] is the maximun width
+      <pre className='mt-2 w-[320px] overflow-x-auto rounded-md bg-slate-950 p-4'>
+        <code className='w-fit text-white'>
+          {JSON.stringify(data, null, 2)}
+        </code>
       </pre>
     ),
   })
